aa754ba81ec8ae9f15281041b68103c9f7cdda89
Amangitrobust <109721359+Amangitrobust@users.noreply.github.com>
2023-02-21 14:42:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css'; 
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+export default class App extends Component {
+  pageSize = 12;
+  render() { 
+    return <div>
+      <Router>
+        <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<News key={'general'} pageSize={this.pageSize} category={'general'}/>}/> 
+            <Route exact path="/sports" element={<News key={'sports'} pageSize={this.pageSize} category={'sports'}/>}/> 
+            <Route exact path="/science" element={<News key={'science'} pageSize={this.pageSize} category={'science'}/>}/> 
+            <Route exact path="/entertainment" element={<News key={'entertainment'} pageSize={this.pageSize} category={'entertainment'}/>}/> 
+            <Route exact path="/business" element={<News key={'business'} pageSize={this.pageSize} category={'business'}/>}/> 
+            <Route exact path="/health" element={<News key={'health'} pageSize={this.pageSize} category={'health'}/>}/> 
+            <Route exact path="/technology" element={<News key={'technology'} pageSize={this.pageSize} category={'technology'}/>}/> 
+          </Routes>
+      </Router>
+    </div>;
+  }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class NewsItem extends Component {
+    render() {
+        let { title, desc, date, imgUrl, url, source } = this.props;
+        return <div>
+            <div className="card my-1">
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1', left:'50%'}}>
+                    {source}
+                </span>
+                <img src={imgUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{desc}</p>
+                    <p className="card-text"><small className='text-muted'>Published On : {date}</small></p>
+                    <a href={url} target="_blank" rel="noreferrer" className="btn btn-dark">Read More</a>
+                </div>
+            </div>
+        </div>;
+    }
+}
